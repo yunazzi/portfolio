3c8794712c5642ea09daac61deedc55f15b05c9d
yunazzi <91316846+yunazzi@users.noreply.github.com>
2024-03-22 23:17:19 +0900

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,32 @@
 //새로고침되자마자 스크롤이벤트 실행하게끔 실행
 $(window).trigger('scroll');
 
-// 인트로 애니메이션
+
+//header
+$('.nav').click(function(){
+    $('html,body').animate({scrollTop:$(this.hash).offset().top})
+})
+//메뉴 열고 닫기
+$('.gnb-toggle').click(function(e){
+    e.preventDefault();
+    $('.gnb-toggle').toggleClass('close');
+    $('.gnb-toggle').toggleClass('open');
+    $('.gnb-area').toggleClass('show');
+    $('body').toggleClass('dimmed');
+})
+//메뉴링크 클릭시 닫기
+$('.nav').click(function(){
+    $('.gnb-toggle').toggleClass('close');
+    $('.gnb-toggle').toggleClass('open');
+    $('.gnb-area').toggleClass('show');
+    $('body').toggleClass('dimmed');
+})
+
 
 //sc-visual
-gsap.to('.sc-visual .visual-details',{
+gsap.to('.sc-visual .detail-area',{
     scrollTrigger:{
-        trigger:'.sc-visual .wrap',
+        trigger:'.sc-visual .intro-area',
         start:'0% 10%',
         end:'0% 0%',
         scrub:1,
@@ -19,39 +39,16 @@ const intro = gsap.timeline();
 
 intro
     .addLabel('a')
-    .from('.sc-visual .wrap em:first-child',{autoAlpha:0,y:100,duration:1,delay:1},"a")
-    .from('.sc-visual .wrap em:first-child .box-line',{"transform": "scale(0)",duration:1.5,delay:1},"a")
-    .from('.sc-visual .wrap em:first-child .box-wrapper',{autoAlpha:0,y:100,duration:1,delay:1.5},"a")
-    .from('.sc-visual .wrap em:nth-child(2)',{autoAlpha:0,y:100,duration:1,delay:1.9},"a")
-    .from('.sc-visual .wrap em:nth-child(3)',{autoAlpha:0,y:100,duration:1,delay:2.3},"a")
-    .from('.sc-visual .visual-details',{autoAlpha:0,y:100,duration:1,delay:2.5},"a")
-
-
-//gnb
-$('.nav').click(function(){
-    $('html,body').animate({scrollTop:$(this.hash).offset().top})
-})
-
-//메뉴 열고 닫기
-$('.btn-menu').click(function(e){
-    e.preventDefault();
-    $('.btn-menu').toggleClass('close');
-    $('.btn-menu').toggleClass('open');
-    $('.gnb-area').toggleClass('show');
-    $('body').toggleClass('dimmed');
-})
-
-//메뉴링크 클릭시 닫기
-$('.nav').click(function(){
-    $('.btn-menu').toggleClass('close');
-    $('.btn-menu').toggleClass('open');
-    $('.gnb-area').toggleClass('show');
-    $('body').toggleClass('dimmed');
-})
+    .from('.sc-visual .intro-area em:first-child',{autoAlpha:0,y:100,duration:1,delay:1},"a")
+    .from('.sc-visual .intro-area em:first-child .box-line',{"transform": "scale(0)",duration:1.5,delay:1},"a")
+    .from('.sc-visual .intro-area em:first-child .box-wrap',{autoAlpha:0,y:100,duration:1,delay:1.5},"a")
+    .from('.sc-visual .intro-area em:nth-child(2)',{autoAlpha:0,y:100,duration:1,delay:1.9},"a")
+    .from('.sc-visual .intro-area em:nth-child(3)',{autoAlpha:0,y:100,duration:1,delay:2.3},"a")
+    .from('.sc-visual .detail-area',{autoAlpha:0,y:100,duration:1,delay:2.5},"a")
 
 
 //sc-content
-gsap.to('.sc-content .area-wrap',{
+gsap.to('.sc-content .area-list',{
     scrollTrigger:{
         trigger:'.sc-content .area1',
         start:'0% 100%',
@@ -63,7 +60,7 @@ gsap.to('.sc-content .area-wrap',{
 
 const contentTl = gsap.timeline({
     scrollTrigger:{
-        trigger:'.sc-visual .wrap em:nth-child(2)',
+        trigger:'.sc-visual .intro-area em:nth-child(2)',
         start:'-30% 0%',
         end:'100% 100%',
     }
@@ -87,7 +84,7 @@ const mainTl = gsap.timeline({
 mainTl.from('.sc-project.main .main-title em:first-child',{autoAlpha:0,y:100,duration:1,delay:1},"c")
 mainTl.from('.sc-project.main .main-title em:last-child',{autoAlpha:0,y:100,duration:1,delay:1.2},"c")
 
-$('.sc-project .link .tags a').hover(function(){
+$('.sc-project.main .link .link-btn a').hover(function(){
     $(this).css({color: "#e0fd60"});
     $(this).children().find('path').css({ stroke: "#e0fd60" });
 },function(){
@@ -107,17 +104,7 @@ const subTl = gsap.timeline({
 subTl.from('.sc-project.sub .main-title em:first-child',{autoAlpha:0,y:100,duration:1,delay:1},"d")
 subTl.from('.sc-project.sub .main-title em:last-child',{autoAlpha:0,y:100,duration:1,delay:1.2},'d')
 
-
-$('.sc-project .content .sub-area a').hover(function(){
-    $(this).css({color: "#e0fd60"});
-    $(this).children().find('path').css({ stroke: "#e0fd60" });
-},function(){
-    $(this).css({color:"#fff"});
-    $(this).children().find('path').css({ stroke: "#fff" });
-})
-
-
-var swiper2 = new Swiper('.content',{
+var swiper2 = new Swiper('.group-pc',{
     slidesPerView:'auto',
     spaceBetween:30,
     pagination:{
@@ -127,11 +114,9 @@ var swiper2 = new Swiper('.content',{
 })
 
 
-
 //sc-contact
 
 //미디어쿼리시 작동여부
-
 if(window.matchMedia("(min-width:769px").matches){ //768px이상
 
     const contactTl = gsap.timeline({
@@ -143,11 +128,11 @@ if(window.matchMedia("(min-width:769px").matches){ //768px이상
     })
 
     contactTl.from('.sc-contact .contact-area .font',{autoAlpha:0,y:100,duration:1,delay:1},"e")
-    contactTl.from('.sc-contact .contact-content .sm-con',{autoAlpha:0,y:100,duration:1,delay:1.1},"e")
-    contactTl.from('.sc-contact .contact-content .line',{width:0,duration:1,delay:1},"e")
+    contactTl.from('.sc-contact .contact-wrap small',{autoAlpha:0,y:100,duration:1,delay:1.1},"e")
+    contactTl.from('.sc-contact .contact-wrap .line',{width:0,duration:1,delay:1},"e")
 
 
-    $('.sc-contact .contact-content .out-title').each(function(i,el){
+    $('.sc-contact .outer-box').each(function(i,el){
         gsap.from($(this),{
             scrollTrigger:{
                 trigger:$(this),
@@ -159,7 +144,7 @@ if(window.matchMedia("(min-width:769px").matches){ //768px이상
             delay:1.2
         })
     },"d+1")
-    $('.sc-contact .contact-content .con-line').each(function(i,el){
+    $('.sc-contact .contact-wrap .con-line').each(function(i,el){
         gsap.from($(this),{
             scrollTrigger:{
                 trigger:$(this),
@@ -176,15 +161,14 @@ if(window.matchMedia("(min-width:769px").matches){ //768px이상
 }
 
 
-
-$('.out-title').mouseenter(function(){
-    $(this).siblings('.in-title').css({opacity:1,transform: "scaleY(1)"})
+$('.outer-box').mouseenter(function(){
+    $(this).siblings('.inner-box').css({opacity:1,transform: "scaleY(1)"})
 })
-$('.out-title').mouseleave(function(){
-    $(this).siblings('.in-title').css({opacity:0,transform: "scaleY(0)"})
+$('.outer-box').mouseleave(function(){
+    $(this).siblings('.inner-box').css({opacity:0,transform: "scaleY(0)"})
 })
 
-var swiper1 = new Swiper(".in-title",{
+var swiper1 = new Swiper(".inner-box",{
     slidesPerView:'auto',
     spaceBetween:100,
     speed:5000,
@@ -197,26 +181,22 @@ var swiper1 = new Swiper(".in-title",{
 
 
 //footer
-
 $('.footer a p').mouseenter(function(){
     $('.footer a img').css({transform:"translateY(0rem)"})
 })
 $('.footer a p').mouseleave(function(){
     $('.footer a img').css({transform:"translateY(2rem)"})
 })
-
 $('.footer a p').click(function(e){
     e.preventDefault();
     window.scrollTo({top:0,behavior:'smooth'})
 })
 
 
-
-//프로그레스 바
+//프로그레스 바 + btn-gotop버튼
 $('.circle').click(function(){
     window.scrollTo({top:0,behavior:'smooth'})
 })
-
 
 gsap.to('.bar',{
     scrollTrigger:{
@@ -227,8 +207,6 @@ gsap.to('.bar',{
     },
     "stroke-dashoffset": 0
 })
-
-//btn-gotop버튼
 
 let lastScrollTop = 0;
 let isFlowslideActive = true;
@@ -253,4 +231,3 @@ $('.btn-gotop').hover(function(){
 },function(){
     $('.btn-gotop').css({opacity:0})
 })
-
